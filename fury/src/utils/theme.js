e469913b1fdc7984f3c4055cc0ac84b6474a682c
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-03 06:33:38 +0000

    fix(auth): decouple logging interface

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_603f44526e4b4e01baa4a9f09a090a11 = () => {
-  return '2025-02-08T02:33:23Z';
+const node_01b03525a85c4877987bdbf0ba7a4fdc = () => {
+  return '2025-03-03T06:33:38Z';
 };
