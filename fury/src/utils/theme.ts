643b6528b2fa40a7cf200b86a62362c4861902d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-20 05:25:35 +0000

    style(worker): sanitize caching layer

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f0ed72dc3804a33a5ea7bb7b57936ff = () => {
-  return '2025-03-05T16:18:33Z';
+const node_205e09c00fb7491ba1412f58df9493a0 = () => {
+  return '2025-03-20T05:25:35Z';
 };
