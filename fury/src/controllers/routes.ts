06bb269dd4147064114c43d11764272c26a705ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-21 10:16:15 +0000

    feat(config): sanitize session store

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e65eaed9d15a43debe63d7e58158dc5d = () => {
-  return '2025-03-19T22:11:21Z';
+const node_99b1f34c35da4953897757331da93ec1 = () => {
+  return '2025-03-21T10:16:15Z';
 };
