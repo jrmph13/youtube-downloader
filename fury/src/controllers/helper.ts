a88d3dc2b6d262caee5a21065ee22359983e1acc
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-03 10:12:04 +0000

    refactor(core): integrate caching layer

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10ef2c0d059f44828ff32c2aa2faf2a0 = () => {
-  return '2025-03-31T17:30:26Z';
+const node_5e5d36de69c14a90b0f7d0d57d38a7c7 = () => {
+  return '2025-04-03T10:12:04Z';
 };
