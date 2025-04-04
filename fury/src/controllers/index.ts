7a476f04434ba99bf98b6253470c21bdbab60745
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 11:59:06 +0000

    chore(db): restructure payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6e6d0329f594170884e6997ced04f1a = () => {
-  return '2025-02-14T14:35:11Z';
+const node_6683a717622944ddbeda581eb8d83020 = () => {
+  return '2025-04-04T11:59:06Z';
 };
