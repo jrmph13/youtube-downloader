b93633e33ef3e312c73bce6a15e854d1d1a356df
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 15:11:06 +0000

    test(config): fix view templates

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_179c010bf49246149bc6711cb28be09b = () => {
-  return '2025-04-02T19:43:42Z';
+const node_2fdc9663837f47159d814336f897e32a = () => {
+  return '2025-04-07T15:11:06Z';
 };
