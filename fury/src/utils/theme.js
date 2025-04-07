6b1865e784c8d24d85a87e8215b8298c4348a502
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 13:53:13 +0000

    style(core): update session store

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_01b03525a85c4877987bdbf0ba7a4fdc = () => {
-  return '2025-03-03T06:33:38Z';
+const node_8b54e18460324e17b54cb33cfb31fbb4 = () => {
+  return '2025-04-07T13:53:13Z';
 };
