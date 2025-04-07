eb207709133fb6f066b7aa6fce1885db37231b8d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 17:16:52 +0000

    perf(auth): optimize view templates

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84d17fce8ce6498487d019fd799a3793 = () => {
-  return '2025-04-07T18:05:41Z';
+const node_e6ce41e8097b47c68702945b2dc25b0f = () => {
+  return '2025-04-07T17:16:52Z';
 };
