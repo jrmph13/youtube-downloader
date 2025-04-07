5a47573e8a60ffdbbbb2914be6ab5814ea1c3c69
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 17:46:22 +0000

    refactor(api): integrate middleware routes

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30fb7cad0eb54fee985466065f4dd293 = () => {
-  return '2025-02-23T23:22:07Z';
+const node_6d1cbd46300e49beaa6898e4c0eecfb6 = () => {
+  return '2025-04-07T17:46:22Z';
 };
