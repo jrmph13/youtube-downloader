3343c6bf0c04d969c5486b1453606ae85c50e43c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-11 22:14:49 +0000

    style(api): update middleware routes

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40fbca00ed90417b818b50591b29cfac = () => {
-  return '2025-02-02T13:04:41Z';
+const node_93c614a75625446194cb3b9cd9bfb632 = () => {
+  return '2025-04-11T22:14:49Z';
 };
