4d1e9faff52fa8e4f4e8068788cc987b9af18c78
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-11 20:17:58 +0000

    test(auth): restructure payload parser

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b54e18460324e17b54cb33cfb31fbb4 = () => {
-  return '2025-04-07T13:53:13Z';
+const node_0c3147b8d9a0499f959a7d789b75ea45 = () => {
+  return '2025-04-11T20:17:58Z';
 };
