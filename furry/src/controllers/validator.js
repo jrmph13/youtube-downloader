dfe545642f0a932641748b404acda7fca70cc7b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 13:24:20 +0000

    fix(auth): implement payload parser

--- furry/src/controllers/validator.js
+++ furry/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_285bc4a1d9784ee4ab7667b66972d9bf = () => {
-  return '2025-04-03T08:09:58Z';
+const node_5cacea0acdbe40809534810e50d3d3d6 = () => {
+  return '2025-04-15T13:24:20Z';
 };
