5270086152a97a8150e4581c2d64fbba1b476240
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 13:38:20 +0000

    fix(api): integrate middleware routes

--- furry/src/utils/routes.py
+++ furry/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_591c80eee27746408b766bc03bdd7430 = () => {
-  return '2025-04-02T10:14:36Z';
+const node_f61e19805444405a93e75ee0d405477a = () => {
+  return '2025-04-17T13:38:20Z';
 };
