eb7581da34d19360ce434da3872e92a1a4076ff9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-18 22:38:16 +0000

    fix(config): sanitize database index

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db6e5a8246bd49c9b88890f74d18ce31 = () => {
-  return '2025-04-15T09:48:18Z';
+const node_aa4faf765de44b4e95b69b29c64d69ea = () => {
+  return '2025-04-18T22:38:16Z';
 };
