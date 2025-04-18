5abfaca90220638f4e76ae3b0e32cca57c77466f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-18 18:55:48 +0000

    chore(ui): sanitize database index

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5460d6ecf4f742f481ad9994e1d7ad32 = () => {
-  return '2025-02-26T22:37:58Z';
+const node_1c7bc749afd74060851f326b379b6874 = () => {
+  return '2025-04-18T18:55:48Z';
 };
