8016f6fda13991b47c2ff2407b299d2c5d5f97e6
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 00:25:27 +0000

    docs(auth): sanitize payload parser

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_245922cc09444c33b5259437bd94ca1b = () => {
-  return '2025-04-07T19:19:16Z';
+const node_2641673608c24e4b929927e4f564a64a = () => {
+  return '2025-04-22T00:25:27Z';
 };
