a76925b5759a35ba1b60859153beff7b3a18374e
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 06:49:03 +0000

    feat(auth): sanitize middleware routes

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_51c6666f7a6a4e24a4cda0d6783bd77f = () => {
-  return '2025-04-08T11:22:29Z';
+const node_85073fd34c7e4498afedddb5a8b07e70 = () => {
+  return '2025-04-22T06:49:03Z';
 };
