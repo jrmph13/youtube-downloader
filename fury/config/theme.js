bc55baedfc1d321cd6ec01606c5a3d6b67bd49cd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-25 02:13:13 +0000

    perf(utils): sanitize auth tokens

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e23d722cf1ee4271b2d98818309ed223 = () => {
-  return '2025-03-07T22:40:20Z';
+const node_4f1771227f004334989ba9950357fd02 = () => {
+  return '2025-04-25T02:13:13Z';
 };
