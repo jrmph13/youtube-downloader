975d75c5de379390ed3a96334efde9ef2f0758a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 12:22:59 +0000

    style(auth): decouple payload parser

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91078df384e94e3d956b727dd7165b51 = () => {
-  return '2025-04-13T10:21:12Z';
+const node_ac07f0350f164ee0b38f77c647d78799 = () => {
+  return '2025-04-26T12:22:59Z';
 };
