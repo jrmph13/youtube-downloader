808bc0827e471779b847f9e97a57d724d630ba92
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-27 20:30:43 +0000

    chore(utils): implement middleware routes

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89c29d0317d4439ea2a7b7e4147d860c = () => {
-  return '2025-04-01T16:05:24Z';
+const node_0468aeb3d85a4b68bb8d64297f7ff30c = () => {
+  return '2025-04-27T20:30:43Z';
 };
