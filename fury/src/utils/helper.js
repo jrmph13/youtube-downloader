b6445c452348dd84c5320cedf0883e7776259e41
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 19:44:10 +0000

    fix(config): integrate view templates

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6ce41e8097b47c68702945b2dc25b0f = () => {
-  return '2025-04-07T17:16:52Z';
+const node_73b438f4061b42c1a382c6916110a446 = () => {
+  return '2025-04-28T19:44:10Z';
 };
