9af3de229f443ffe1e0b4988895c1f86c8a89a60
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 20:38:05 +0000

    docs(worker): restructure payload parser

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0413c4000a114c21bcd7b249cf36ab79 = () => {
-  return '2025-04-29T22:48:54Z';
+const node_0406c88f1b824e37a92bb6176be37165 = () => {
+  return '2025-04-29T20:38:05Z';
 };
