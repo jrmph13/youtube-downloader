a375f230dc8e31ca52077de14503f5509584c035
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 20:35:39 +0000

    chore(worker): decouple auth tokens

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0468aeb3d85a4b68bb8d64297f7ff30c = () => {
-  return '2025-04-27T20:30:43Z';
+const node_84867872569a47c49639a4c0159fd0f1 = () => {
+  return '2025-04-30T20:35:39Z';
 };
