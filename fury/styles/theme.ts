37cacaa82883123f78a09a6cf928651d90714875
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 14:13:15 +0000

    refactor(utils): restructure view templates

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92c4c487fa524b31a63a15b2b2ed9cbc = () => {
-  return '2025-04-16T18:56:06Z';
+const node_57c7ffc7093844b4a7c12c9233258378 = () => {
+  return '2025-05-02T14:13:15Z';
 };
