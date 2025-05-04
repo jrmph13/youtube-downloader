160ac5f82b9d0e258f7d4e6c7527bbb33736ed63
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 18:16:28 +0000

    fix(worker): enhance payload parser

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b2618614565749628ead4747d3a0b60b = () => {
-  return '2025-03-28T21:44:55Z';
+const node_6629df1647134bf7b1503e540581d228 = () => {
+  return '2025-05-04T18:16:28Z';
 };
