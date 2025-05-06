f35ddd582207b1922e44949ea9bf7a7ef46444d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-06 15:50:27 +0000

    style(auth): optimize view templates

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16f8571358a148479fc70905002d484e = () => {
-  return '2025-04-04T07:16:58Z';
+const node_045c776eccce4283a08d21c652de86f6 = () => {
+  return '2025-05-06T15:50:27Z';
 };
