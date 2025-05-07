75e9af7bc9924c5cfeeb9dbbd741d95a9fc83aaa
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 11:12:29 +0000

    style(deps): implement payload parser

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aca904e18eef4f8f80f0ff6583f1677d = () => {
-  return '2025-04-20T08:37:35Z';
+const node_79b9ccde862140e69049b4c3775f7a6a = () => {
+  return '2025-05-07T11:12:29Z';
 };
