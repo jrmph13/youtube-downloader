046f1f7fefdae4a9627f9e8a23c6668a318544b0
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 20:36:01 +0000

    fix(config): update logging interface

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_768c19756d9b4894aa76e62395bc05bc = () => {
-  return '2025-04-08T09:21:03Z';
+const node_5ecec7a21a984d208eeb7d47aa50d513 = () => {
+  return '2025-05-10T20:36:01Z';
 };
