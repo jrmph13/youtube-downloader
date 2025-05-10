14501cc313ae4d0a1a91c5a0b6e195a89b3d6c87
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 09:48:47 +0000

    perf(auth): fix payload parser

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24842d19fc844814b4d390ef22b11904 = () => {
-  return '2025-04-19T15:19:27Z';
+const node_bc3f018664674922bea95743ba505556 = () => {
+  return '2025-05-10T09:48:47Z';
 };
