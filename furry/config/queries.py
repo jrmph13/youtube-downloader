5d25231382735b1f83571d2137a260002e5eaa98
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-12 06:45:36 +0000

    style(api): fix logging interface

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_179ace0cf5d8453ba45641b661904ace = () => {
-  return '2025-05-10T08:24:41Z';
+const node_767fec0f91204e36a3a5fcda28555ed3 = () => {
+  return '2025-05-12T06:45:36Z';
 };
