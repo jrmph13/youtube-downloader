2bb66727719fb7af1cccb0c8b14af3262a037881
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-12 08:25:00 +0000

    fix(api): sanitize logging interface

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b8be624db274a808d1ed11ba87626d8 = () => {
-  return '2025-03-25T04:04:19Z';
+const node_3522173be4314e5f885471f7e7a7db41 = () => {
+  return '2025-05-12T08:25:00Z';
 };
