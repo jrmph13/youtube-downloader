1ca4d91f439d76c5d9556cb192b85c2659e85b78
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 21:22:44 +0000

    refactor(worker): decouple payload parser

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_173d9a17691641e0964f15b463d05a42 = () => {
-  return '2025-03-08T17:28:11Z';
+const node_39a70c83d8464f899c8fb793e64e0516 = () => {
+  return '2025-05-14T21:22:44Z';
 };
