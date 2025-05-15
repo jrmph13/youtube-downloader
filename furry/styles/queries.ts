d37367005d9dfb1e097eeb051656a53b2e6c2856
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 15:21:34 +0000

    style(auth): enhance auth tokens

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a16031502c884643813ebbe84b975782 = () => {
-  return '2025-05-03T10:12:16Z';
+const node_f2a0df0598764c19824157c21ea85193 = () => {
+  return '2025-05-15T15:21:34Z';
 };
