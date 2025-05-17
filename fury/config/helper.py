c1d39649016394a86a346269ea7b037f8dc49c92
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 09:59:44 +0000

    style(config): decouple view templates

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2be93793b2284693ba3a3bb8201428ff = () => {
-  return '2025-04-01T11:13:48Z';
+const node_8322250e98874b42b8d792a97c14497e = () => {
+  return '2025-05-17T09:59:44Z';
 };
