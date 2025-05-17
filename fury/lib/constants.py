7331e248edea0372724f9fa0061f064096c8badd
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 14:59:28 +0000

    feat(auth): enhance payload parser

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_717830c4d1884ff3b4a43024b5b5e37d = () => {
-  return '2025-04-11T14:22:06Z';
+const node_e579f0f1464d4aab85d2700ea180ba45 = () => {
+  return '2025-05-17T14:59:28Z';
 };
