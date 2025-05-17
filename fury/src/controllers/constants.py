e9dfcc53a7c267662d6b04efb624c7604b08c2cd
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 15:49:21 +0000

    fix(worker): decouple session store

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3ed2adef0f14631a2cfd6ca0a3df9ab = () => {
-  return '2025-05-15T06:40:55Z';
+const node_38fa81bcf0ab428ba763eed3d99efe32 = () => {
+  return '2025-05-17T15:49:21Z';
 };
