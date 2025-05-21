f2bd86e25cff75a81abf6367681fd1400b6cf1cb
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-21 17:40:24 +0000

    chore(deps): fix logging interface

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e0fc5611f4a40f79632d4ef4617c444 = () => {
-  return '2025-05-15T03:51:47Z';
+const node_7c98fb830c064f399268f6ba0242eea0 = () => {
+  return '2025-05-21T17:40:24Z';
 };
