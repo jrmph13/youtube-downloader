1cc1a0d5d354984ce2c4aa0fc4248b849b1a1419
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 13:27:45 +0000

    test(utils): sanitize view templates

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6cc4d2034514c78afd7d713d79e0900 = () => {
-  return '2025-05-10T10:14:13Z';
+const node_3f32606356b8428590803c9df7b01875 = () => {
+  return '2025-05-25T13:27:45Z';
 };
