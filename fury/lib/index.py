81f2e3d59bb888d54c0a4e7bb996825218168087
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 03:51:00 +0000

    style(ui): decouple logging interface

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cb05fffec1948ddb04eaf59791bf8c4 = () => {
-  return '2025-05-19T02:47:59Z';
+const node_8468c34d15cb4a8d80188c841993ffee = () => {
+  return '2025-05-29T03:51:00Z';
 };
