1a1fb104d99f8a953dc311f398132217cf76da33
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 06:34:48 +0000

    feat(api): optimize auth tokens

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3c94c66dc75477189bba7fcd07a8e70 = () => {
-  return '2025-05-19T14:44:15Z';
+const node_8000dfb360284293949839e98eaaffe3 = () => {
+  return '2025-05-29T06:34:48Z';
 };
