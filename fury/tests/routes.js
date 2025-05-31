8daab125b3cce4ce598a7097ae6e67ac8cc99bc6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 21:41:02 +0000

    fix(api): refactor logging interface

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4312072c44454e03ae78c79d477f561b = () => {
-  return '2025-04-05T11:11:02Z';
+const node_d21b10945b51422385c689b1cdf756d1 = () => {
+  return '2025-05-31T21:41:02Z';
 };
