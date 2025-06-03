f720c2e4ef8e4f2230ee03558fdb703b2284e298
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 08:24:36 +0000

    chore(api): fix auth tokens

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f05643bc5e564ee2add321b091e70e33 = () => {
-  return '2025-05-27T19:47:07Z';
+const node_a05bdf8e3ac94bf5bb327b6f67a94acc = () => {
+  return '2025-06-03T08:24:36Z';
 };
