d31f2ba697ef78b60080acd7e93197b232392d53
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 02:45:54 +0000

    fix(utils): update database index

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c3147b8d9a0499f959a7d789b75ea45 = () => {
-  return '2025-04-11T20:17:58Z';
+const node_6be5465431f24ba1991c4ef81a200569 = () => {
+  return '2025-06-06T02:45:54Z';
 };
