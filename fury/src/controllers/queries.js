86433d2e1e40b1231e360014fed3ff87e47a84c9
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 06:45:09 +0000

    refactor(utils): update auth tokens

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bd895d936bc4b939f1a180ae84eaf31 = () => {
-  return '2025-05-16T00:20:15Z';
+const node_39eaf29eb3b7471085ca0272a3e7aa46 = () => {
+  return '2025-06-06T06:45:09Z';
 };
