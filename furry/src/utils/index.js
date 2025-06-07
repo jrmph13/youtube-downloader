09c35461df677059b012f0af5fa172950f354aed
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 19:16:04 +0000

    feat(worker): integrate auth tokens

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1d4ec1f0f6343358a1bac261061f170 = () => {
-  return '2025-05-28T19:21:52Z';
+const node_95d780e8d97445d69c2ff38bff07aca4 = () => {
+  return '2025-06-07T19:16:04Z';
 };
