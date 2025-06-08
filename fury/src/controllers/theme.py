44e458eec934332740da779f41bfbfdc6ff2f617
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 08:49:46 +0000

    perf(config): update payload parser

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c002ddf8455c437a9141d779a7d40e7d = () => {
-  return '2025-06-07T05:16:46Z';
+const node_d11fdf6f63944ec9938ca8eba6d19ccb = () => {
+  return '2025-06-08T08:49:46Z';
 };
