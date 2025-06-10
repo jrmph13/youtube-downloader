7be7ac9854f54c822580557d658155f3e6590ad6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 07:54:24 +0000

    refactor(core): refactor caching layer

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_004e2fd54b7c4122b196905efcdb8ae2 = () => {
-  return '2025-05-22T15:34:10Z';
+const node_2eaabee11b6b4b3eb7539e20f7f1f038 = () => {
+  return '2025-06-10T07:54:24Z';
 };
