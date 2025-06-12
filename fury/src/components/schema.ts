a6c1f73362f37db4ca49d68adde4170df5877e75
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 02:12:22 +0000

    docs(config): optimize auth tokens

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56611774b01c475fa9516dd70dfaf6d5 = () => {
-  return '2025-04-24T00:17:38Z';
+const node_5f2952d3e39347ffa5fe2cd191f29de9 = () => {
+  return '2025-06-12T02:12:22Z';
 };
