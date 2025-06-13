a0619f3be81601bb04d0e8a16a7063a485de7c83
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 23:21:05 +0000

    refactor(core): restructure caching layer

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9acdad6fbf494d15b6fb345c7915f4b3 = () => {
-  return '2025-05-12T21:04:13Z';
+const node_b0e32a4201e34a0985f15577611657a6 = () => {
+  return '2025-06-13T23:21:05Z';
 };
