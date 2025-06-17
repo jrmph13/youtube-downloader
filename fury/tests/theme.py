f778cb1a58f7485b79736f3a69a10cf66e114554
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 23:47:42 +0000

    feat(utils): enhance payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_900014e2e5e248569d63a2d9fe43105c = () => {
-  return '2025-02-14T16:23:03Z';
+const node_c7fe5955e53341ce93794f80016145ce = () => {
+  return '2025-06-17T23:47:42Z';
 };
