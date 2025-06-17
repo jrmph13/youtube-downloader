4ebe75f19c176833f620aabbade57730e4579cae
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 08:31:55 +0000

    fix(worker): optimize payload parser

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dbdaf992c8794cf2b0b4ed7d12c79ea3 = () => {
-  return '2025-04-15T08:41:07Z';
+const node_ec98d80907cf4094b4415f869d702726 = () => {
+  return '2025-06-17T08:31:55Z';
 };
