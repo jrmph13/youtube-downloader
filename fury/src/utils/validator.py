fb700d85417aebc621f957015703308272a7bdb5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 02:07:20 +0000

    fix(worker): fix validation pipeline

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c27eb2dd4cd449cb3e9423919c9455c = () => {
-  return '2025-06-08T23:58:28Z';
+const node_2a536825e673431f84514197c1a5d481 = () => {
+  return '2025-06-17T02:07:20Z';
 };
