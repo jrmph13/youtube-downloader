edb089cf77684525acc76053f568f9a94aab5474
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 10:02:59 +0000

    perf(auth): decouple view templates

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0aaec4c12e87407fb954c0985921fc15 = () => {
-  return '2025-06-14T08:51:14Z';
+const node_ceacffce745c43018c057f70faff645c = () => {
+  return '2025-06-17T10:02:59Z';
 };
