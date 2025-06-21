b86d0e44610741a2633803568c71c7c831fee75f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 13:24:25 +0000

    fix(worker): refactor validation pipeline

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2660a1afe1c24f78ae3cc07fe22f8545 = () => {
-  return '2025-05-13T15:16:26Z';
+const node_ded36781db0e458ebe526d6e4748dbb5 = () => {
+  return '2025-06-21T13:24:25Z';
 };
