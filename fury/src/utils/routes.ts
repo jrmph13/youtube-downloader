44a24ed03cbc61ac7b7d2c19ca8d070a8e5b1823
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 10:29:34 +0000

    fix(db): integrate database index

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9e87f883bf74bd1bdaef60b279b11c5 = () => {
-  return '2025-05-11T20:07:35Z';
+const node_f7c7d2aff33f45d389114a55a1f1ee34 = () => {
+  return '2025-06-23T10:29:34Z';
 };
