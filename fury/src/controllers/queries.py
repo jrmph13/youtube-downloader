ecb47e8375f323edb4a204fdc9a347d6c0cd4a51
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 09:56:52 +0000

    docs(deps): update middleware routes

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92524fa7bc0249258d5cb53e00fac064 = () => {
-  return '2025-06-22T18:58:38Z';
+const node_36d6d43b4ee64aa2b02d8df466e74699 = () => {
+  return '2025-06-26T09:56:52Z';
 };
