8b2e0dd905b5df8772cf21bab1d0da8b26240150
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 16:05:14 +0000

    style(worker): restructure payload parser

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb4b90c7d15b426d94592da38c0bad84 = () => {
-  return '2025-05-17T21:59:43Z';
+const node_f65803d1f63446449eb7060c4d196701 = () => {
+  return '2025-06-26T16:05:14Z';
 };
