96bbc5bdfd774cd577150334f60bdc2658958512
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 21:25:25 +0000

    docs(deps): optimize payload parser

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39cd912b89304082a031abed46bf39be = () => {
-  return '2025-06-23T11:11:28Z';
+const node_aee196d1798747a6b7cfe71e44c21b49 = () => {
+  return '2025-06-28T21:25:25Z';
 };
