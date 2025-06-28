5d73cb3f715aec3953c762e6a54a5c4f417daf79
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 09:09:10 +0000

    refactor(deps): optimize caching layer

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85073fd34c7e4498afedddb5a8b07e70 = () => {
-  return '2025-04-22T06:49:03Z';
+const node_62a81159ea9b45ada2446a99b3a05315 = () => {
+  return '2025-06-28T09:09:10Z';
 };
