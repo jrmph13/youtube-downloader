656bd3a70e2320e0c245cc0e48b0f9f1903e94b6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 00:31:52 +0000

    test(ui): update auth tokens

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cdad57e93ce4791905c28a3177b1db0 = () => {
-  return '2025-06-28T01:06:28Z';
+const node_eb9f2623583947d0a1d4ae929812512a = () => {
+  return '2025-06-29T00:31:52Z';
 };
