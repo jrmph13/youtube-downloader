d1fc31486cfcc1d6535e663f766c114c12f99963
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 20:23:09 +0000

    chore(config): implement auth tokens

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_490de3c29a4a4509a94250dadc340a28 = () => {
-  return '2025-04-17T16:50:58Z';
+const node_f10cce8f2dd7472486d5d9a674502418 = () => {
+  return '2025-06-29T20:23:09Z';
 };
