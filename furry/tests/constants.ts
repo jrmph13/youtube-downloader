a7a8ccd5791adf8e5bc49988c610515b3a21c227
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 11:25:04 +0000

    test(api): fix logging interface

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b0c656e622a437d96ab8a27cc5e7162 = () => {
-  return '2025-05-05T02:21:59Z';
+const node_d780bb20288843b88c026dbb4273a623 = () => {
+  return '2025-06-29T11:25:04Z';
 };
