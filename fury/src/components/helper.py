fbd25cad422c688cd5d1036d5fb685b1bc992ecf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 13:19:02 +0000

    fix(ui): optimize session store

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d61b4a2e541241eba0d6a29684b1df21 = () => {
-  return '2025-04-14T02:49:54Z';
+const node_12b0d0729e494ae591048d9164c351e7 = () => {
+  return '2025-06-30T13:19:02Z';
 };
