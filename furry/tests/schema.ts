d3dbc8566f6c5e9c81dba54300f9966540996e9b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 15:44:10 +0000

    style(db): decouple database index

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8402a32a170408da5c7a80773099672 = () => {
-  return '2025-04-01T20:59:25Z';
+const node_4c22e80a6f324f6d8e156bea5bf34787 = () => {
+  return '2025-06-30T15:44:10Z';
 };
