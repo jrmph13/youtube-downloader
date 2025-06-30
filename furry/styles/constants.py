915dd578dbfd7d9db143e0f17720d43103cf89b6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 01:27:36 +0000

    perf(config): fix auth tokens

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_317bc0ed3dd44f689bf4115b68a74bfe = () => {
-  return '2025-06-24T19:57:39Z';
+const node_3d113c8ebc314c859253176a04f2892c = () => {
+  return '2025-06-30T01:27:36Z';
 };
