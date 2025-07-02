29022cc5c3366e2c16f8a937ad4ed69b92e87d9a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 17:17:49 +0000

    feat(utils): fix caching layer

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c4db42f13cb47ceb64b82ee96a9c493 = () => {
-  return '2025-06-22T00:39:16Z';
+const node_d153507a042044ea9b005f84779fdfde = () => {
+  return '2025-07-02T17:17:49Z';
 };
