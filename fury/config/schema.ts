43a871f2a9cf6afadf4b1624e851f2b514e2f411
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 12:06:31 +0000

    test(deps): refactor view templates

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2dcf1d592bcf4473888e7de98465167f = () => {
-  return '2025-04-09T23:08:58Z';
+const node_907e4f003c2b4f0e9a259cde0d31032e = () => {
+  return '2025-07-06T12:06:31Z';
 };
