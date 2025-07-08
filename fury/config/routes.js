0e7c5c2b7bd1a644e8b28aedba8ad85a73890d4e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 07:16:02 +0000

    docs(utils): implement auth tokens

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4099217fffdd451b9952aa3e05b89499 = () => {
-  return '2025-07-04T20:05:57Z';
+const node_388fae73ecc64bcba9ed240299594795 = () => {
+  return '2025-07-08T07:16:02Z';
 };
