c950b3556ed813c913c941f3420915d5a052d0e7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-11 20:25:02 +0000

    perf(config): update logging interface

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf1b8e88ca0941f19824cb283a1e57ac = () => {
-  return '2025-06-07T19:20:32Z';
+const node_b7cc8186511f491fb18aefac6ee3abd1 = () => {
+  return '2025-07-11T20:25:02Z';
 };
