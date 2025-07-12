48c1e08d4dd022092199f78bbfdb81b74fd5915b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 04:56:39 +0000

    perf(utils): fix payload parser

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fda0da912baf404e9818460f9b7e0513 = () => {
-  return '2025-04-14T18:50:07Z';
+const node_3269ee70063145a3b6ea28f38939afd8 = () => {
+  return '2025-07-12T04:56:39Z';
 };
