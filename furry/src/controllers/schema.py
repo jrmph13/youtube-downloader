58992b9a881a216f9cd1e8f519f276250279afc1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 14:35:26 +0000

    feat(auth): restructure database index

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a14ef344df14a61b7e186ff9302a54d = () => {
-  return '2025-07-14T14:07:08Z';
+const node_02c0c27e99214184af54bfaacb278d26 = () => {
+  return '2025-07-14T14:35:26Z';
 };
