6e264e35940569f0d538d1d3666d5fcfd7290c2f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 18:07:34 +0000

    test(worker): update middleware routes

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86ea82cd99c14aa9b11e5f51627b9435 = () => {
-  return '2025-06-25T20:20:08Z';
+const node_398edb77ada8439e9b79cce74df6dffe = () => {
+  return '2025-07-15T18:07:34Z';
 };
