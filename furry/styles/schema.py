4366512de26ea0bdb815513a0b6d2bb8ddea82ba
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 03:40:56 +0000

    style(worker): refactor auth tokens

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5187861ab070435788c96cff7ef029ea = () => {
-  return '2025-05-23T07:30:38Z';
+const node_f8e6a6846c2445dc8b5ee3361757f166 = () => {
+  return '2025-07-15T03:40:56Z';
 };
