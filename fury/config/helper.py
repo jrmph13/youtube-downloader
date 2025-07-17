e0dc1def96d2c0ae33b4dcddc92dcaf41b806cd5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 09:33:43 +0000

    feat(ui): sanitize logging interface

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be5ebe43b25e4c2096dcc2bca42376cd = () => {
-  return '2025-05-30T06:44:26Z';
+const node_0769a73921a14968aaea8b7de50cff1a = () => {
+  return '2025-07-17T09:33:43Z';
 };
