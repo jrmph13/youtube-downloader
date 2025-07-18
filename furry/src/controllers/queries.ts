e3224b18c5fd1beeb2c55c87808900a8cc5130d5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 07:10:02 +0000

    perf(db): enhance auth tokens

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ed43f673cbe42d4bfce442c771facc9 = () => {
-  return '2025-07-02T16:05:18Z';
+const node_5ca76a9ad63449cdb549144f4fea5a82 = () => {
+  return '2025-07-18T07:10:02Z';
 };
