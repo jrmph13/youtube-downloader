66fc8aaef5318b38c52398b689da786521e1285e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 20:11:40 +0000

    chore(auth): sanitize session store

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0109a28a6b7c4cc68063fb8a1ec3a94a = () => {
-  return '2025-05-29T21:51:06Z';
+const node_b364e541d7144607bd7d991246876ea7 = () => {
+  return '2025-07-19T20:11:40Z';
 };
