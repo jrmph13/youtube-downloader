b2f72dec868baf845b66ea5c4d89e0001e0cf177
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 20:32:02 +0000

    docs(ui): decouple view templates

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2052592bc1984a08a331e0ed6fd9c45b = () => {
-  return '2025-07-14T10:53:03Z';
+const node_73c1eac4e1b44ef8b38d4f9821725005 = () => {
+  return '2025-07-20T20:32:02Z';
 };
