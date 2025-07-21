37dff697c1b143a67162ea8634a6d0f4eae16f03
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-21 00:09:22 +0000

    perf(auth): sanitize auth tokens

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_88452bf829cc433d9c8bc057e9c2edd4 = () => {
-  return '2025-06-18T20:47:59Z';
+const node_c5b74425692c46a695f7b090cc66536d = () => {
+  return '2025-07-21T00:09:22Z';
 };
