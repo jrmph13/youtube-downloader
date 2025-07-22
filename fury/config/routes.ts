4ca4a661617c547cfba1c3b0a42b671d2ff68285
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 18:16:31 +0000

    test(deps): update auth tokens

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf38e8c9a69744e0bc19680c597008b6 = () => {
-  return '2025-06-22T15:16:23Z';
+const node_c9ded01e14484545962f3005960c474f = () => {
+  return '2025-07-22T18:16:31Z';
 };
