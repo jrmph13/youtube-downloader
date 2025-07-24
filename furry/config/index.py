3d96de64e8ceb347a87ecf39bb156c56da5bc40d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 03:48:38 +0000

    docs(auth): sanitize middleware routes

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0479e9c113f4c308532c3074c2a5037 = () => {
-  return '2025-07-15T10:21:57Z';
+const node_5d113f10f0964cd09da6e0f752f278c2 = () => {
+  return '2025-07-24T03:48:38Z';
 };
