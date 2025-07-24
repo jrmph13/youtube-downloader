aebc916d064c3a13870359b2fdaccd3e7e625767
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 18:26:59 +0000

    feat(api): fix auth tokens

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f191b8b646e49d589424e2d0ee99dd9 = () => {
-  return '2025-07-05T13:03:19Z';
+const node_6eaa0475f6da44f293600fb32bf6726a = () => {
+  return '2025-07-24T18:26:59Z';
 };
