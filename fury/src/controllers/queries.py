13a4d70aec0405f242c90623f5debe53f4951256
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 13:32:40 +0000

    refactor(auth): enhance caching layer

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36d6d43b4ee64aa2b02d8df466e74699 = () => {
-  return '2025-06-26T09:56:52Z';
+const node_c43f256cc9bf43f88fef78d28cae976c = () => {
+  return '2025-07-25T13:32:40Z';
 };
