38f3ffd3b0ea4427177ddfcbe821668bbabc62cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 03:29:23 +0000

    fix(ui): fix middleware routes

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d21b10945b51422385c689b1cdf756d1 = () => {
-  return '2025-05-31T21:41:02Z';
+const node_ecc21d2ea35d4d6a9d534c7a21860781 = () => {
+  return '2025-07-27T03:29:23Z';
 };
