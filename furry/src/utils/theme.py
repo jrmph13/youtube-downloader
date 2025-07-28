4142ec434975ee249462297e0ef7d4fe02917dee
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 00:35:33 +0000

    refactor(config): optimize payload parser

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec98d80907cf4094b4415f869d702726 = () => {
-  return '2025-06-17T08:31:55Z';
+const node_756616a65a504d72842516f47d09f1bf = () => {
+  return '2025-07-28T00:35:33Z';
 };
