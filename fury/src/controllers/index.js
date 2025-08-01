71e7748e73aab04875f527fe5d8eef2d79b8ae4c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-01 02:03:04 +0000

    fix(auth): sanitize validation pipeline

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d95fee8f22524f9ba831a53a3388d72f = () => {
-  return '2025-07-31T09:22:00Z';
+const node_c648a0c70d85489b8d686aaa9f813920 = () => {
+  return '2025-08-01T02:03:04Z';
 };
