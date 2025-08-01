32740d443ec803f84fa5ecaf4d7a77a58dcf835e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-01 17:00:49 +0000

    feat(db): decouple auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05e9a33e6c4d49539dd66c9bd00a1a14 = () => {
-  return '2025-07-18T12:25:15Z';
+const node_b25f99db3d0d42a38b5a0433f381b09b = () => {
+  return '2025-08-01T17:00:49Z';
 };
