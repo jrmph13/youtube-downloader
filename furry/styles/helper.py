79b8dc5228a09af4423f3f4b04ec41a6c301629a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 12:48:26 +0000

    refactor(auth): optimize payload parser

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f3cbda3886a4bdf88dc483b27e6ca6f = () => {
-  return '2025-06-21T02:32:28Z';
+const node_6bd7e4423cf34c58aab8ca847cbc6df4 = () => {
+  return '2025-08-02T12:48:26Z';
 };
