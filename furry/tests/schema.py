79dfb966f5cbc53519dcb80e263c0beb30b2057f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 22:56:42 +0000

    perf(ui): enhance payload parser

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77a1d3f26767406aa3f357a0649be21b = () => {
-  return '2025-08-01T09:34:37Z';
+const node_ce4899230a2c41cdb3473713cdff347a = () => {
+  return '2025-08-04T22:56:42Z';
 };
