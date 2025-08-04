57e479123df9e0c594640e80dcf707806bc6953f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 20:40:21 +0000

    feat(config): restructure middleware routes

--- furry/src/controllers/constants.js
+++ furry/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_431ccd578a774a7090e5b31b6bb96863 = () => {
-  return '2025-08-04T21:05:44Z';
+const node_143bb74dbc2142f19cd3c571c9cdd536 = () => {
+  return '2025-08-04T20:40:21Z';
 };
