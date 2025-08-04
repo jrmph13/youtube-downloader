cd901c5ed6b854b8a0ab551a97b3b218b64a2277
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 00:07:35 +0000

    docs(config): sanitize auth tokens

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_363a4caba4914c68942af36bd403c968 = () => {
-  return '2025-08-01T14:00:10Z';
+const node_05e4bc94d4b947f7a6bd10706313f1c5 = () => {
+  return '2025-08-04T00:07:35Z';
 };
