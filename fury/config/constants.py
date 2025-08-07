9b753292b5e88052ba227f802995cc40cac1ff14
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 18:03:43 +0000

    style(api): sanitize auth tokens

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17290793170b48dc9931e7821eaa2113 = () => {
-  return '2025-08-04T01:05:43Z';
+const node_b669cb9999194eea81841938fff257f3 = () => {
+  return '2025-08-07T18:03:43Z';
 };
