bad96a073e980cd686c1528856038ebb172d971b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 03:00:10 +0000

    fix(auth): refactor database index

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5fe7a46f531146f3bde10acb72116269 = () => {
-  return '2025-04-11T07:17:46Z';
+const node_d2c5c559fcac4c1cb451ad0b2c8eb51e = () => {
+  return '2025-08-08T03:00:10Z';
 };
