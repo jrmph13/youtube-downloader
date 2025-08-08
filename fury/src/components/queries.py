4a1f89a07fca375ea49c15378318d3c79007e294
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 15:49:28 +0000

    style(core): refactor payload parser

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8e5b481e50d4d8384630242c729ecf4 = () => {
-  return '2025-07-02T21:55:26Z';
+const node_d4281fcbf4eb44e495d94d032098dbd1 = () => {
+  return '2025-08-08T15:49:28Z';
 };
