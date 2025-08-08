f298504e3939e9f20d283e3c0cf6b406f68310f5
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 22:11:29 +0000

    refactor(config): refactor view templates

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06148d41fbab4027a8bd86083d510445 = () => {
-  return '2025-07-30T15:41:58Z';
+const node_69e1b20129a94df782e485d7e4753056 = () => {
+  return '2025-08-08T22:11:29Z';
 };
