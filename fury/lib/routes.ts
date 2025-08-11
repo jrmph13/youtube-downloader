2d2522ce0dbfd9cccf5a80da54e279c6c24bf9b1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 09:58:46 +0000

    perf(auth): restructure validation pipeline

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7568307cfe342aebb116f959301cefd = () => {
-  return '2025-06-16T18:59:18Z';
+const node_aff8f2206b7c4abcbed4543986fffce1 = () => {
+  return '2025-08-11T09:58:46Z';
 };
