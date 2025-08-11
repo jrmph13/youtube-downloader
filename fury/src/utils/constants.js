9b9105cbef43b7ff8f8350f961e515501c80594c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 09:40:58 +0000

    chore(api): update database index

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fb7cf36cf8a48969bdbbb86dcfb6a0d = () => {
-  return '2025-08-03T14:10:02Z';
+const node_b588cadd52c247389b8778038f6a9d94 = () => {
+  return '2025-08-11T09:40:58Z';
 };
