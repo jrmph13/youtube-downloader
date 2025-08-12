6ccae06b3e500dfb6f8ace290198aaf2d1bca09c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 08:57:13 +0000

    refactor(worker): fix auth tokens

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_574c503c6b9e47efb83719af64a76f44 = () => {
-  return '2025-07-17T23:15:25Z';
+const node_dc17276382b84390a5e9386900e635a0 = () => {
+  return '2025-08-12T08:57:13Z';
 };
