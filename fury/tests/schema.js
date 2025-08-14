6481120d287ec6241d5a35efbbd722d6797ec64a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 14:29:58 +0000

    docs(config): optimize payload parser

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_565e2796015e41fe9f73749432e32614 = () => {
-  return '2025-06-09T18:12:40Z';
+const node_04c4e0b36ac2439985c0d320c575bafb = () => {
+  return '2025-08-14T14:29:58Z';
 };
