015486ad4c13a97887ae5c2706944f08648fa5a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-21 02:02:36 +0000

    fix(core): fix middleware routes

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_195cf5acd75f4cc58bfd6047f405b8bc = () => {
-  return '2025-08-05T21:15:31Z';
+const node_df37c7802b234c9784b838fc51f637bd = () => {
+  return '2025-08-21T02:02:36Z';
 };
