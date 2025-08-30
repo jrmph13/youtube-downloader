4f20a1f760c777230e7be77e6f017da82f1b20a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 10:51:31 +0000

    fix(ui): implement auth tokens

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0be2aca0820472c9afbeca9f8d916fa = () => {
-  return '2025-07-22T11:31:51Z';
+const node_f045cde30c724e869f62a3f41230c6d7 = () => {
+  return '2025-08-30T10:51:31Z';
 };
