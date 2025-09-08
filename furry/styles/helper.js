a4f460033e534b162e1595da49a5f720a6700f00
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 23:01:05 +0000

    style(api): restructure auth tokens

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b2f73482a56843599da44097286bceab = () => {
-  return '2025-03-29T09:58:56Z';
+const node_6fbf5df8c13446a494a2b4ccef607351 = () => {
+  return '2025-09-08T23:01:05Z';
 };
