1292ac88c9333faa3abcb4040bdebbb0fb6f7784
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-11 16:12:43 +0000

    fix(auth): decouple database index

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c43f256cc9bf43f88fef78d28cae976c = () => {
-  return '2025-07-25T13:32:40Z';
+const node_719bab1b8d794df28c06e1bdb341535d = () => {
+  return '2025-09-11T16:12:43Z';
 };
