a34b7fa71ddb18f8d4979cce6f5cc17ed5379b1c
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-17 09:44:45 +0000

    perf(api): integrate middleware routes

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ffa66966428f4820a8383e109c50a1cb = () => {
-  return '2025-07-24T22:23:23Z';
+const node_6d6265929d174266bfba365a0b2c224e = () => {
+  return '2025-09-17T09:44:45Z';
 };
