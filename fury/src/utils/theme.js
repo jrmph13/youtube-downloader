20cb6e6932b62aa550eca7ffdae6c410ba0a6a7a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 10:22:04 +0000

    test(deps): decouple auth tokens

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_869f1b8491d747d49d5747f3f4b9c7fa = () => {
-  return '2025-08-26T07:26:21Z';
+const node_7b2d76f5cae8476da69c73814491c12a = () => {
+  return '2025-09-23T10:22:04Z';
 };
