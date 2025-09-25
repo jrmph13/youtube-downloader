abf498a3a3b2743a05f25267004fc69ddfa81f23
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 17:58:38 +0000

    feat(ui): fix auth tokens

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36e9cd7c93f34e2aa421fb59e17e9bd7 = () => {
-  return '2025-08-03T07:30:39Z';
+const node_e770a7e4436445c7b9e2357da9027fbd = () => {
+  return '2025-09-25T17:58:38Z';
 };
