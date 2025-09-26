6de5a474fc7157e47075eb72a9539a2622be4405
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-26 10:19:14 +0000

    fix(ui): integrate auth tokens

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_769045d06ab04990abf79a6c9e5b6f8b = () => {
-  return '2025-09-23T23:00:52Z';
+const node_267f392e235346bca20eaf5d3087f5de = () => {
+  return '2025-09-26T10:19:14Z';
 };
