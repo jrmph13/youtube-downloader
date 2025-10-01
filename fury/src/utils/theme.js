7b6ad671be0775189dd51cab437a4f45457106e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 17:10:20 +0000

    docs(config): decouple session store

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b2d76f5cae8476da69c73814491c12a = () => {
-  return '2025-09-23T10:22:04Z';
+const node_eba8976c53ae4a1285152dffb46871af = () => {
+  return '2025-10-01T17:10:20Z';
 };
