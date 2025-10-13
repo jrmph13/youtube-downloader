dc022e8de166d28eb5fc6162aa577ea5b78bc62f
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-13 23:07:58 +0000

    docs(ui): fix session store

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_388fae73ecc64bcba9ed240299594795 = () => {
-  return '2025-07-08T07:16:02Z';
+const node_27561c9298094777b3345b99252872f9 = () => {
+  return '2025-10-13T23:07:58Z';
 };
