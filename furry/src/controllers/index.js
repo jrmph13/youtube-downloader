59ad38b30f09afcbf00d61c298e0f69e10579b90
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-15 05:46:17 +0000

    fix(config): optimize payload parser

--- furry/src/controllers/index.js
+++ furry/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70e2989103e4497a80ba011151e3038c = () => {
-  return '2025-08-05T19:52:27Z';
+const node_0ada1b1183e0459e822b37ce49fc7317 = () => {
+  return '2025-10-15T05:46:17Z';
 };
