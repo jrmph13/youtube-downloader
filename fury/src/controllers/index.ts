d4eb5a0e69073a318d995ea57c5a281f05e7f847
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-15 04:57:30 +0000

    refactor(core): optimize session store

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b96c76343f9f4c23ba4a53f038ded6d4 = () => {
-  return '2025-10-03T15:52:08Z';
+const node_106bd894d5d44c5fb680c5c3eea9df71 = () => {
+  return '2025-10-15T04:57:30Z';
 };
