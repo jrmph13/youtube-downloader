aaa861f9981ad6be64bb7fb78289928baa89a526
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-16 04:50:59 +0000

    perf(core): decouple logging interface

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_edadc06b0fdc4e7cb0f3469451f6424b = () => {
-  return '2025-08-03T01:28:57Z';
+const node_e7292b2516b54c829cc4c253c3621f18 = () => {
+  return '2025-10-16T04:50:59Z';
 };
