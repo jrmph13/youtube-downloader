25c828919207f420ac7dd8158f36851b9c9e1222
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-19 18:38:38 +0000

    docs(utils): decouple middleware routes

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2592d7484584781bab5d36160104fcf = () => {
-  return '2025-08-15T08:21:15Z';
+const node_98ea8eee5d314628846b1186e28413e7 = () => {
+  return '2025-10-19T18:38:38Z';
 };
