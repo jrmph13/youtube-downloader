93e36308bc6c45dd91a20e3f364e500c0c407d30
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-23 15:03:57 +0000

    style(api): optimize payload parser

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b46794080a44d7b967c39a647ab0559 = () => {
-  return '2025-07-28T23:34:00Z';
+const node_8aeb7c55aaa74c3d9e7d3accd6abc0a1 = () => {
+  return '2025-10-23T15:03:57Z';
 };
