53cff8a466ff88ff7f87ae5f8b6f3b3d69432d83
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-04 11:47:56 +0000

    docs(config): update logging interface

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6fb7bf05fef4c82a944295205b5b2e9 = () => {
-  return '2025-10-21T23:38:49Z';
+const node_298ba91c457e485481c627fb37f3cbab = () => {
+  return '2025-11-04T11:47:56Z';
 };
