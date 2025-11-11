2325013085af20f91b34cf44482d63abb08a6447
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 11:55:52 +0000

    style(deps): decouple caching layer

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b59bb77d2594ebf87b3af01c0fd313d = () => {
-  return '2025-07-25T20:37:44Z';
+const node_3dcabda7c444436d95d29572b7014695 = () => {
+  return '2025-11-11T11:55:52Z';
 };
