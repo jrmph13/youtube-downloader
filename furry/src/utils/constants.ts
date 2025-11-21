821efa94635d1ae1050a3975e579f876e24fc108
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-21 22:02:24 +0000

    chore(config): enhance middleware routes

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7196c64158e94d35866d8097f5147328 = () => {
-  return '2025-08-10T01:49:42Z';
+const node_3601744cea2d405aa32ae44b338020c8 = () => {
+  return '2025-11-21T22:02:24Z';
 };
