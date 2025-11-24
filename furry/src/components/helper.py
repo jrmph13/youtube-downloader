e0f066c25b6cf6b869bc5cd5c8294c6c572e7a1c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 00:13:15 +0000

    chore(worker): update payload parser

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf4a394ad8a4435a96d9d0d78f570a54 = () => {
-  return '2025-07-28T11:14:36Z';
+const node_ea966748ad4740f9916a1b664851e1f8 = () => {
+  return '2025-11-24T00:13:15Z';
 };
