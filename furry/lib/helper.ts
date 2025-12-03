31cd22de12f4658716def80fc3be2d93ecdab188
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-03 10:25:41 +0000

    refactor(ui): sanitize payload parser

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b2b0c28a077496a8af175b22c68ed7e = () => {
-  return '2025-09-27T19:57:41Z';
+const node_c477db2a41db471fb7f6e755d1140d5b = () => {
+  return '2025-12-03T10:25:41Z';
 };
