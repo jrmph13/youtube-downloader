77806e9b00b307bdfbc70fc06ea3afe77091e617
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 17:52:25 +0000

    fix(worker): restructure validation pipeline

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93997691e25f49a283e789174d58a827 = () => {
-  return '2025-07-18T20:42:26Z';
+const node_23c629d3adda458ebb9ef15b716519aa = () => {
+  return '2025-12-08T17:52:25Z';
 };
