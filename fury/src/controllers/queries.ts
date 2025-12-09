aad380eaeffce7b92550a51ae477028a9b758b8a
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 09:43:00 +0000

    chore(db): optimize caching layer

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_018f17f6f579476ea46fd4ca8d0374ef = () => {
-  return '2025-08-14T23:46:16Z';
+const node_cae4b666fd40490d95251af86587d213 = () => {
+  return '2025-12-09T09:43:00Z';
 };
