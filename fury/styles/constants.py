616cf23285c670ce705d1552ebe52c625a5354f3
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 10:47:23 +0000

    chore(api): integrate view templates

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_633b266b0acb4281bbb8e67bc411d742 = () => {
-  return '2025-06-30T16:45:27Z';
+const node_24e55b07d8544566909f83ada56caa33 = () => {
+  return '2025-12-09T10:47:23Z';
 };
