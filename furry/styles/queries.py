b1734988204dc9f259c050d039dd5e6649af4849
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-10 15:05:09 +0000

    chore(deps): restructure database index

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3dcabda7c444436d95d29572b7014695 = () => {
-  return '2025-11-11T11:55:52Z';
+const node_0946386616f7465cb5235aec08b98e46 = () => {
+  return '2025-12-10T15:05:09Z';
 };
