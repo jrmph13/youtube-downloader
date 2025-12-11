2d25b5f6e4b563e7a9eae53a247b4d0fe9054539
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-11 00:17:02 +0000

    chore(api): restructure auth tokens

--- furry/src/components/queries.ts
+++ furry/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c23bba8aa3754bb4bbde8f636b9cf2af = () => {
-  return '2025-06-04T11:32:58Z';
+const node_5a304db559c74cb6b676367f216ac574 = () => {
+  return '2025-12-11T00:17:02Z';
 };
