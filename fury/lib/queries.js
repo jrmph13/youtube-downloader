ad3226aee16ce68875802ed2775fbb296dc32d1b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-11 22:48:05 +0000

    fix(core): restructure logging interface

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34ab5a7ed23a4915bce6d8fadd3710b3 = () => {
-  return '2025-10-24T13:23:25Z';
+const node_b06ca3af021643aab2630f0404ecf5cb = () => {
+  return '2025-12-11T22:48:05Z';
 };
