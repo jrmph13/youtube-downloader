600b30d6290508cc900da40bde71cc6559f2a68e
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 21:16:26 +0000

    fix(ui): update auth tokens

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ceb84be1d61a40a189eae1b6a06821c2 = () => {
-  return '2025-09-24T13:33:09Z';
+const node_4fd6dd313013483ea06daf8af42aae2c = () => {
+  return '2025-12-17T21:16:26Z';
 };
