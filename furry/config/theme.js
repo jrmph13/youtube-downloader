b9b82ec2156bd9c1c22663b57caa0e5c4abb0e0e
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 10:52:14 +0000

    docs(ui): update view templates

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f537abf54c76459f8a070a875f70a8d6 = () => {
-  return '2025-09-09T07:14:01Z';
+const node_a5d38730c9554a7a9df92b07d7d7799b = () => {
+  return '2025-12-25T10:52:14Z';
 };
