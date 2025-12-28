968e52146c2a82c9a1539aab41ab8d9c300c21f5
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 19:50:11 +0000

    chore(deps): sanitize view templates

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37c2cc9c6f5e4cd5b4ed2bf37dabd4ea = () => {
-  return '2025-11-19T19:20:08Z';
+const node_521fc46839134b38a6e88ea494a8c607 = () => {
+  return '2025-12-28T19:50:11Z';
 };
