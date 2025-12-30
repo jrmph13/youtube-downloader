204ee5d421afa28de2e23b3ec4ae6258924a936b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-30 12:56:27 +0000

    fix(auth): refactor session store

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08d993a489b04938b29d4a3a8cd0b80d = () => {
-  return '2025-10-28T03:21:05Z';
+const node_a4c6099e471c4f12ae80fc0b75bb0f32 = () => {
+  return '2025-12-30T12:56:27Z';
 };
