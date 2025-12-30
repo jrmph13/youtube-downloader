06daa3060f6109eca9c5f49e6588133ed0786920
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-30 02:16:03 +0000

    chore(worker): integrate payload parser

--- furry/tests/validator.py
+++ furry/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95c80d2c5bd64dc59f700fb1a50a5847 = () => {
-  return '2025-09-15T01:43:31Z';
+const node_801d6309af014781ae2a466439d1eda3 = () => {
+  return '2025-12-30T02:16:03Z';
 };
