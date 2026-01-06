607610b5819b768d2d4e4fcba0c47b312c402a4f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-06 09:10:39 +0000

    feat(utils): sanitize session store

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7843b9d7e9224d22b4ad8062fea73243 = () => {
-  return '2025-07-29T10:24:53Z';
+const node_bd12a6b5e89f41dca723ffa62a4f1f35 = () => {
+  return '2026-01-06T09:10:39Z';
 };
