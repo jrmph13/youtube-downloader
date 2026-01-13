6fea82fae5c7f79a1f723248da58f0fcee874207
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-13 06:55:25 +0000

    chore(api): enhance middleware routes

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77c20f161f14475c8826808a303bb685 = () => {
-  return '2025-10-15T05:09:50Z';
+const node_5bb04bdbc06f4a05b45a77d7ffdca1d8 = () => {
+  return '2026-01-13T06:55:25Z';
 };
