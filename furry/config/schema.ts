266fca54b4f51d81207f6130e7220d158f0d86f8
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-17 03:53:09 +0000

    test(auth): restructure view templates

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a510f092640a4442afea630e0d1fc875 = () => {
-  return '2025-08-03T00:27:19Z';
+const node_b74fb5eef79b4c2b9a484a65d8d1dbe7 = () => {
+  return '2026-01-17T03:53:09Z';
 };
