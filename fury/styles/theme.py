2595c0cb2c3ae672778fcab624ba6d67269ee3ac
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 14:17:42 +0000

    test(api): update validation pipeline

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59728bfae6454d67a5340649cc8e0d80 = () => {
-  return '2025-06-27T00:47:03Z';
+const node_9454790b806f47289207628afa87f1f9 = () => {
+  return '2026-01-24T14:17:42Z';
 };
