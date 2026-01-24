bec5877bf54dd9085960dd83c7f2152f57313baa
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 02:19:18 +0000

    chore(api): enhance payload parser

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd12a6b5e89f41dca723ffa62a4f1f35 = () => {
-  return '2026-01-06T09:10:39Z';
+const node_149c80e9a47e435a8e75a11814e36cc0 = () => {
+  return '2026-01-24T02:19:18Z';
 };
