524b6df5717606367c413e92ae43b8b379eb094f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 20:06:34 +0000

    style(auth): sanitize session store

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7048bc586a04612bf3ad53baa807a9f = () => {
-  return '2026-01-23T05:15:34Z';
+const node_ff5de15486d9491f9ac1ede69aa253d3 = () => {
+  return '2026-01-24T20:06:34Z';
 };
