b58eda1a2509599e84df85d9b3962bea2505465a
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 12:55:23 +0000

    test(ui): restructure middleware routes

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dae8b67c5df948fe869e405462631f95 = () => {
-  return '2025-09-11T12:48:24Z';
+const node_e6a1ea12bf78438f8b4924862688e888 = () => {
+  return '2026-01-24T12:55:23Z';
 };
