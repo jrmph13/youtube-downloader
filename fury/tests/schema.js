21bbb15b178522098c6b60cc4418c6eb4f81a322
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 09:50:52 +0000

    test(ui): restructure auth tokens

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cadf4c78027949af99e3d719a612c836 = () => {
-  return '2025-12-17T06:00:35Z';
+const node_8017f1ad79f34d308660062bb2e21b02 = () => {
+  return '2026-01-27T09:50:52Z';
 };
