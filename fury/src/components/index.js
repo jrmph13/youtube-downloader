5b84d7428515dddf33ac55df91b635320f21a419
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 18:01:46 +0000

    refactor(core): optimize auth tokens

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b3ce2926ddf43d795676bb7dc7e7b58 = () => {
-  return '2026-01-29T17:57:30Z';
+const node_dd327880c79148ac88730872a0bc21bf = () => {
+  return '2026-01-30T18:01:46Z';
 };
