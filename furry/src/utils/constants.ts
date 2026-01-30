6d33e9f4ea137794f8356fbc79ea5ac1ba45c170
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 07:32:10 +0000

    refactor(auth): sanitize view templates

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6c0b7732622430b9190fa1a6f95536a = () => {
-  return '2026-01-20T08:16:49Z';
+const node_691f288ec4d9488280d552fe029b7dce = () => {
+  return '2026-01-30T07:32:10Z';
 };
