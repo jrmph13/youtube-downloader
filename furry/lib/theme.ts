c463bc29bdb485375d049d6393f53a390ec32039
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 02:04:33 +0000

    perf(db): sanitize view templates

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef8c5ebb230e4defafeaf526dcf15527 = () => {
-  return '2025-10-13T19:55:51Z';
+const node_faf8e3dbada84fe0831bed1226c2e919 = () => {
+  return '2026-02-03T02:04:33Z';
 };
