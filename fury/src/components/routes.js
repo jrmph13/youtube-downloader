e0e520a0049624768a0ca3309312844638862e28
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 19:29:12 +0000

    feat(worker): restructure middleware routes

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed04c4e7306c4e71a5a883c59bb141ae = () => {
-  return '2025-06-28T12:03:37Z';
+const node_6398b064352243169dbf8b693aea1b72 = () => {
+  return '2026-02-03T19:29:12Z';
 };
