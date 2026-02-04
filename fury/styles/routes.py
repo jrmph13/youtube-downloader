ba2f3ca22802d2835c356eeddac286aeaaf6d05f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 05:01:08 +0000

    docs(config): update auth tokens

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_01b6ff657e3045f5ab2e35bcc4d1e09d = () => {
-  return '2025-06-24T04:54:55Z';
+const node_8fd7c250e7df4e9bb2bf26b8ca8f7950 = () => {
+  return '2026-02-04T05:01:08Z';
 };
