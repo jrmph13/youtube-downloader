3c62fc80f44016e99615a3ce543427f088701aba
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 11:49:04 +0000

    fix(auth): refactor view templates

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f0abcd1f7ad24104b9dc76d40a38c196 = () => {
-  return '2026-02-02T14:26:39Z';
+const node_c17c974249004d00a0fbd09df016f543 = () => {
+  return '2026-02-05T11:49:04Z';
 };
