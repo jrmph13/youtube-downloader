d462ec407bdf2c0a62e1f4518a4fd5a85592905b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 14:28:20 +0000

    docs(api): restructure database index

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_816e7624a9024fe0b9433b44c0bbd0ef = () => {
-  return '2025-12-02T13:35:53Z';
+const node_6a57e0d70a944795b667bf0a0aef1255 = () => {
+  return '2026-02-06T14:28:20Z';
 };
