0c4d4a3fc3cefb6e74acaeb60e7ebe473923a741
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 13:09:02 +0000

    style(utils): update payload parser

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b41c8d73bb944049b0f25adc699bce2 = () => {
-  return '2026-02-02T13:53:17Z';
+const node_44403a1a74e8445ab52480846a44ca83 = () => {
+  return '2026-02-09T13:09:02Z';
 };
