14b0f6715d6886759b73fcf50d0b47eda5ad8266
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 21:51:28 +0000

    test(api): restructure auth tokens

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac78589ac50c4b61be90f8a3e6533c98 = () => {
-  return '2025-11-11T02:42:18Z';
+const node_e5c0035e6b4847cea1a4ad6334fda13f = () => {
+  return '2026-02-12T21:51:28Z';
 };
