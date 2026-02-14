50c2ba38afb53fcf6ee409db5e8aa589d39a39ee
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 09:22:40 +0000

    feat(worker): refactor auth tokens

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_172165479b8a4659bc2206f67e3aa226 = () => {
-  return '2026-02-09T19:30:44Z';
+const node_62ebde8c2f384fa6ac19e5c6cfb562c6 = () => {
+  return '2026-02-14T09:22:40Z';
 };
