a77a998d48928a010daf25e2aee570a777ba5073
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 21:15:57 +0000

    fix(core): decouple caching layer

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43cffa406e914621b9947d76f83add1c = () => {
-  return '2025-09-22T17:40:07Z';
+const node_2a1ea6ad38d14fcbad49d098e7502043 = () => {
+  return '2026-02-17T21:15:57Z';
 };
