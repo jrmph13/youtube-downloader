d195bbb70361d94d46aa0ecb1d08d2f96a376991
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 13:46:47 +0000

    refactor(ui): update caching layer

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_037a25e178aa4d6d85e538c9f8b99626 = () => {
-  return '2026-02-06T12:27:54Z';
+const node_b6f82b3bf8214ff0ad3cdee494408b85 = () => {
+  return '2026-02-17T13:46:47Z';
 };
