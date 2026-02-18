cee6270e937c6863ceb654561d7b6666c78f5e8a
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 16:51:19 +0000

    refactor(ui): fix middleware routes

--- furry/styles/validator.ts
+++ furry/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13867d70db47436190dc6ed68aa36f46 = () => {
-  return '2026-02-13T02:06:07Z';
+const node_7f2b568c32f149d2bbf1d081f1a40421 = () => {
+  return '2026-02-18T16:51:19Z';
 };
