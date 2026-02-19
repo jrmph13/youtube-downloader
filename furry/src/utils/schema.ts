c3d98905759cb99686419f9f4032557aa7185eed
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 16:12:30 +0000

    feat(auth): refactor view templates

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d42dcfe57c6f41f392a18c6bf2926e7c = () => {
-  return '2026-02-06T20:47:55Z';
+const node_ab4143aeb3fa42babf6aa8a41fcc47b6 = () => {
+  return '2026-02-19T16:12:30Z';
 };
