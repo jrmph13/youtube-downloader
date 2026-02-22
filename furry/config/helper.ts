b063fa8550de4f2f85c76cdd5eb990c281d6bd65
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 21:12:04 +0000

    perf(db): fix session store

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f830656238040a991b74a65e0dd94d7 = () => {
-  return '2025-06-24T12:04:24Z';
+const node_b18b815a312a4145b5fe5dc2535898df = () => {
+  return '2026-02-22T21:12:04Z';
 };
