31859e2f378eef0733888dfa43ac8e2c556270e3
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 09:52:53 +0000

    perf(auth): restructure middleware routes

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a5e03d189314b90923fd25cac6e85d0 = () => {
-  return '2026-02-14T03:22:23Z';
+const node_13f4132e123f4c90b99da7991a571c22 = () => {
+  return '2026-02-25T09:52:53Z';
 };
