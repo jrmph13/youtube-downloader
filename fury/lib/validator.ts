8e257d5bfaf2d5e5f4be60804cae68ca1976c021
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 20:38:18 +0000

    refactor(worker): update session store

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6737554dc6b74bb7987670b5c6349e0f = () => {
-  return '2026-02-18T03:29:18Z';
+const node_9c7be426818f4a20a4f1020b1cd0126b = () => {
+  return '2026-02-25T20:38:18Z';
 };
