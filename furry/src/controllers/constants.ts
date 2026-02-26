fb900ae6024fd8d14c17e1968c2066d0a07deeb6
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 14:11:42 +0000

    refactor(ui): sanitize session store

--- furry/src/controllers/constants.ts
+++ furry/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5725ae93b2974753b7e4660bb370c5b4 = () => {
-  return '2026-02-23T22:18:28Z';
+const node_77239085da464662ba208e89f5c7f43d = () => {
+  return '2026-02-26T14:11:42Z';
 };
