5387798bd89034dace3d52e18e771a16fae4ecc9
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 06:05:01 +0000

    chore(worker): sanitize auth tokens

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17e19bdde55f42c3af9d6f595af2fb40 = () => {
-  return '2026-02-23T05:40:28Z';
+const node_0aaf145a31d04f859310c0ff022647e5 = () => {
+  return '2026-03-06T06:05:01Z';
 };
