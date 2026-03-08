0cd8b96d2e5dbb1e0914926e9f8c1486fe070c26
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 13:28:47 +0000

    fix(config): update session store

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8dd618e1c6784e64922bd68e3253eaab = () => {
-  return '2026-03-05T14:01:51Z';
+const node_a902e1d1535e4c2682b30b70dfa27533 = () => {
+  return '2026-03-08T13:28:47Z';
 };
