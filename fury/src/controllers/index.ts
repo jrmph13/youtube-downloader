a496bbadddfbf93abac7b97a41991adc84cd6818
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 06:08:41 +0000

    test(core): optimize middleware routes

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_845424a52f574f42a0740c2ca5092d18 = () => {
-  return '2026-02-26T09:32:02Z';
+const node_483c1b57ac2648fcbc128cec0607dece = () => {
+  return '2026-03-12T06:08:41Z';
 };
