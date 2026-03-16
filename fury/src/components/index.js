1512515abb75069cc574b120bbf753fe1a560fec
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 00:08:51 +0000

    refactor(core): integrate auth tokens

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_68d8231130dd4af98bfb1b6fbe573ce6 = () => {
-  return '2026-02-02T05:16:55Z';
+const node_e5ac5bccbe294cada6ea08862c43697b = () => {
+  return '2026-03-16T00:08:51Z';
 };
