d82d2e9c4ff25e2b5ce3e583912a70db0072fc50
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 10:40:46 +0000

    fix(auth): integrate session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2eaabee11b6b4b3eb7539e20f7f1f038 = () => {
-  return '2025-06-10T07:54:24Z';
+const node_f19996f23b124306aa641d3b70a4de81 = () => {
+  return '2026-03-18T10:40:46Z';
 };
