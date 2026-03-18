61c816bfa33dfe0e242b5866a50ba317e2827e9e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 16:55:54 +0000

    test(auth): update payload parser

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6104cec8b4d4f10a5b77e4aba37a717 = () => {
-  return '2026-03-12T16:12:03Z';
+const node_fc45a7e2676e4a3e808ad35e1b735f1b = () => {
+  return '2026-03-18T16:55:54Z';
 };
