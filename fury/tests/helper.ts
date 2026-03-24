9026a3d19372a5f60a006002313e7115ede6ca12
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-24 17:56:17 +0000

    feat(db): sanitize middleware routes

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71eb795dc002492fbf8496e0b48c409b = () => {
-  return '2026-02-10T00:33:24Z';
+const node_683f393f364d4ef2b1579183bff97aff = () => {
+  return '2026-03-24T17:56:17Z';
 };
