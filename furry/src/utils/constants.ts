955400e86c1e0690812fecb897b89e588b338c55
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-24 19:28:21 +0000

    feat(core): enhance auth tokens

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2bc424ce50f0434f8b1ab1d7e804bc7c = () => {
-  return '2026-03-11T01:10:58Z';
+const node_08821fa47f3c4c5392c5be5fc3779eec = () => {
+  return '2026-03-24T19:28:21Z';
 };
