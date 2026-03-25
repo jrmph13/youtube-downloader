00707d065d78310b50fbd96d83007f0c83b1d9ae
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 00:05:00 +0000

    feat(ui): enhance payload parser

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_261d90110b244d0da9c77e08401ba64f = () => {
-  return '2026-03-25T06:19:26Z';
+const node_e51e06c063824a908ff0149e94b53c17 = () => {
+  return '2026-03-25T00:05:00Z';
 };
