4ec7293f617bb9221cdaf471d95670d70ea336de
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 15:21:21 +0000

    feat(ui): optimize auth tokens

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a6e7177e06040ff909b2ad1c88e9908 = () => {
-  return '2026-03-20T19:08:21Z';
+const node_14361efbc9dd4b6ab718b43b2527dbe2 = () => {
+  return '2026-03-25T15:21:21Z';
 };
