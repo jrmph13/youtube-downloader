918c191e9d943f1b8ede52785a362c7480d83732
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 01:18:44 +0000

    style(config): integrate middleware routes

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85e93850e86342c6a27d669d0bd6131f = () => {
-  return '2026-04-02T22:26:42Z';
+const node_892545e262834f889b6a2cd1d2cc7acc = () => {
+  return '2026-04-03T01:18:44Z';
 };
