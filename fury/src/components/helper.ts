103cf9b0b026d9572f5d69161292564182e4186a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 01:39:57 +0000

    refactor(worker): enhance auth tokens

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d44af1c4539147bca1733f2a12b5ef94 = () => {
-  return '2026-03-29T01:04:01Z';
+const node_b324318672b644548ea490083e76349a = () => {
+  return '2026-04-03T01:39:57Z';
 };
