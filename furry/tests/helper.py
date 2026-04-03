c714febf3e4d79968a69f1cb2ada8dbfdf3c9338
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 01:44:53 +0000

    perf(ui): refactor database index

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93b5774a7985497c9dc33d70328b3b0f = () => {
-  return '2026-03-31T20:12:04Z';
+const node_4d46e6da24e44435a24ed18f97e4b3bd = () => {
+  return '2026-04-03T01:44:53Z';
 };
