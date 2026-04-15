90cdd124652e0760b5d7fc8dd9cdfdd6124a2d1c
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 13:03:26 +0000

    perf(db): decouple payload parser

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb3a9110c03a471298e3c5c91328343a = () => {
-  return '2025-08-12T10:17:05Z';
+const node_54ab0755fdd048169f4b67a4b39a90c1 = () => {
+  return '2026-04-15T13:03:26Z';
 };
