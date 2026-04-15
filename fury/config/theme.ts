ae3ad53962e1f00a7ed97f316eb8c7ccbc116d70
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 10:26:41 +0000

    feat(deps): update auth tokens

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3005f310476544faa47d7f4e9f15c56d = () => {
-  return '2026-01-26T02:26:24Z';
+const node_8d8e05d1d5744f0fa0b539bd1b34e235 = () => {
+  return '2026-04-15T10:26:41Z';
 };
