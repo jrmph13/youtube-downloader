4786fc14ed920f64196efbbf583bc6e3c14717c0
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 13:42:41 +0000

    refactor(core): restructure auth tokens

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05fd98bad220411481455b99cda4d945 = () => {
-  return '2026-04-02T05:21:49Z';
+const node_10a65ff7c9514e23ae40bc0f6745c90a = () => {
+  return '2026-04-16T13:42:41Z';
 };
