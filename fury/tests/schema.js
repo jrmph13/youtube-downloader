a1427652d3d9bfc29b798addeed5166f41ccd2c5
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 01:02:39 +0000

    refactor(db): decouple auth tokens

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8110592a13b445ed8ee7c43e8f47eb69 = () => {
-  return '2026-01-28T23:51:17Z';
+const node_5364dfa8770040649424e6113f247c51 = () => {
+  return '2026-04-17T01:02:39Z';
 };
