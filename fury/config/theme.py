b08fc8dd68b5e7f95a880437921adb118b5d754b
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 12:58:16 +0000

    perf(utils): decouple middleware routes

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8888e5d231804ec99252f9a2db85148e = () => {
-  return '2026-02-18T19:29:18Z';
+const node_6e68d5ad877f4d1f9d46ce6dd55ca3fe = () => {
+  return '2026-04-21T12:58:16Z';
 };
