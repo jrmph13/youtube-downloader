c03b02f2fd5f02407872406b3e11eac2e226696a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 00:30:16 +0000

    fix(worker): decouple payload parser

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee55eb1b95bc4c3eac02fbe5c0653c6d = () => {
-  return '2026-02-22T05:57:27Z';
+const node_ba39ec30992c45c394faa0b1e3796ddb = () => {
+  return '2026-04-23T00:30:16Z';
 };
