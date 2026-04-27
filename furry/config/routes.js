6c7d2856e8f49d7c223920a2e6d4e2414f118a32
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 18:06:02 +0000

    style(auth): decouple database index

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57dfe8c4085649c081bc5037d6634e65 = () => {
-  return '2025-12-09T08:36:40Z';
+const node_f94a22207b744341a362039edda24718 = () => {
+  return '2026-04-27T18:06:02Z';
 };
