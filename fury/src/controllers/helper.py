e997ece1085f9fd28fa6ecfdffb0758d016e46a3
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-28 16:56:44 +0000

    refactor(ui): decouple view templates

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e61a9163c904cedbf3f662ce52a8b36 = () => {
-  return '2026-04-17T04:36:56Z';
+const node_39fc372a74614432a024cd98286ea604 = () => {
+  return '2026-04-28T16:56:44Z';
 };
