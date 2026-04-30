6c7936f7c2f8fed1575f1e7a4e306f77139fffcc
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 15:08:16 +0000

    fix(core): restructure auth tokens

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cd318783ef643f288fc6c74bde7bee0 = () => {
-  return '2026-04-15T17:34:39Z';
+const node_22afce5c0cda43279611d9b0c05023d5 = () => {
+  return '2026-04-30T15:08:16Z';
 };
