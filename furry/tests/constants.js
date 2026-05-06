97d24af42cec2e9ebeb3134efc4574147d535a3f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 11:11:41 +0000

    perf(config): fix middleware routes

--- furry/tests/constants.js
+++ furry/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96667e9594384ac6b2a8f1544fdddf9a = () => {
-  return '2025-06-25T08:12:12Z';
+const node_8459fc0c2feb4ed2a764b51278b6779b = () => {
+  return '2026-05-06T11:11:41Z';
 };
