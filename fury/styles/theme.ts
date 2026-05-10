809b0794a588c3931f2d8107f8b6e6caa40891ea
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 03:12:25 +0000

    style(deps): implement middleware routes

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e1732794f3a48f89cc3d71358c09fab = () => {
-  return '2026-05-06T12:58:56Z';
+const node_6866e04a55e24348b07ff9c8990a21aa = () => {
+  return '2026-05-10T03:12:25Z';
 };
