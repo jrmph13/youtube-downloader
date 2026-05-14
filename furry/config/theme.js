e1d6bc551029632b80dc341ac3d000da0989b771
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 16:12:59 +0000

    refactor(api): fix database index

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5d38730c9554a7a9df92b07d7d7799b = () => {
-  return '2025-12-25T10:52:14Z';
+const node_fa97dc4be5e54156be60ccd2de42b69d = () => {
+  return '2026-05-14T16:12:59Z';
 };
