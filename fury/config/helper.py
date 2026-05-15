b54b75800024a23a088068f68fd41633e847fc9b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-15 21:19:06 +0000

    docs(auth): restructure database index

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_861f3059aac848b792c39dc4b9b1eb81 = () => {
-  return '2026-04-30T21:17:01Z';
+const node_d19b3b49aea040e2b3927d3f37c2503d = () => {
+  return '2026-05-15T21:19:06Z';
 };
