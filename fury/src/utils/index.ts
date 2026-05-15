e9c83ac9030f3d762608cefdf17c1475bcd1edfe
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-15 08:16:55 +0000

    fix(deps): integrate view templates

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ded36781db0e458ebe526d6e4748dbb5 = () => {
-  return '2025-06-21T13:24:25Z';
+const node_1a8a3e3f7ab443b4953d101432aaa9fb = () => {
+  return '2026-05-15T08:16:55Z';
 };
