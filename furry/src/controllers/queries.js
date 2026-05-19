40f620aa6f729d58e2bc9a8d8f4d7f85a24d2f93
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 00:26:17 +0000

    fix(auth): restructure payload parser

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b52df8f731e04f5c86a4146a07a7081a = () => {
-  return '2026-05-19T23:43:54Z';
+const node_36258ba603a749dd96fc2f858956cf28 = () => {
+  return '2026-05-19T00:26:17Z';
 };
