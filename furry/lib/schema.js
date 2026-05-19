77528ba0ba072d6884a15fed807724c8931c8f08
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 07:00:59 +0000

    fix(core): integrate payload parser

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd9105eaa8b943b78baa7e3b0c9bc327 = () => {
-  return '2026-03-27T02:55:38Z';
+const node_6c801cb4cc0d4a3aa337acbfbf0983d5 = () => {
+  return '2026-05-19T07:00:59Z';
 };
