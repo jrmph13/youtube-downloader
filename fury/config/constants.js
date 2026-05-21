aa5ff9d84135ddeb04b32de56b83430487c0e09a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-21 21:04:42 +0000

    refactor(auth): enhance middleware routes

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11e2afb0c28d47a7b9f47ae48a46a0fe = () => {
-  return '2026-05-13T18:10:43Z';
+const node_c6c449bfc554468fbc828e948c8e5288 = () => {
+  return '2026-05-21T21:04:42Z';
 };
