3a4b8e99fd7d762369d1d190807f5baca712c110
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 00:04:45 +0000

    perf(ui): restructure auth tokens

--- furry/src/controllers/queries.py
+++ furry/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_855e60806e59405bae848c4befc80330 = () => {
-  return '2026-04-24T20:20:38Z';
+const node_a50cc499cecf4a27b74183e393ae0f29 = () => {
+  return '2026-05-27T00:04:45Z';
 };
