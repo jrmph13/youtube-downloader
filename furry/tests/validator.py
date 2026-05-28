411597bdc7fb0377e31a597a0fc684373235679f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 16:22:16 +0000

    feat(utils): fix auth tokens

--- furry/tests/validator.py
+++ furry/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3df569bd55cf4a73bf9077329b06d992 = () => {
-  return '2026-05-26T07:27:00Z';
+const node_a118121622eb4d6d8fe96fdd57a54c8c = () => {
+  return '2026-05-28T16:22:16Z';
 };
