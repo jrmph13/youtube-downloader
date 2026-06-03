8ba56198158d255abd52174e472718de905a8dd1
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 05:45:34 +0000

    chore(auth): optimize middleware routes

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab4143aeb3fa42babf6aa8a41fcc47b6 = () => {
-  return '2026-02-19T16:12:30Z';
+const node_80c9066784544dcda6d8bc4439dc1c5f = () => {
+  return '2026-06-03T05:45:34Z';
 };
