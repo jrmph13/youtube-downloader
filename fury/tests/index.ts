c0871a7634444ffa1760a523682cdff2ca1fd1d1
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 08:10:01 +0000

    perf(deps): update payload parser

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c1bdcc207574cc6a604b1939a28f1ff = () => {
-  return '2026-04-13T09:10:22Z';
+const node_8ffb37e30f944e2ca4385a0e525fc597 = () => {
+  return '2026-06-04T08:10:01Z';
 };
