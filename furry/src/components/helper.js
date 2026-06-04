c4eec237b7de86cfc9a9d91343b5575c3e2bc9ed
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 00:00:43 +0000

    fix(ui): fix session store

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41418ff6f88944ac926f50b449217193 = () => {
-  return '2026-06-01T21:38:14Z';
+const node_1324b07e31314a4fa42180565d694fd1 = () => {
+  return '2026-06-04T00:00:43Z';
 };
