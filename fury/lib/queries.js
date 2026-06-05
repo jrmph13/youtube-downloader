581ba0c02fe76e4fd1e81ba4d0b8eb5c869e5850
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 18:45:36 +0000

    perf(api): fix middleware routes

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f2943c0e50d4323a65adccf393c0907 = () => {
-  return '2026-06-02T09:14:07Z';
+const node_6270ee71904f442fbd1de4eeecc20b72 = () => {
+  return '2026-06-05T18:45:36Z';
 };
