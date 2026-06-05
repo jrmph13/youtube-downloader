42c38450ccfe5adac06ad8ece937ce01e928d70e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 06:40:26 +0000

    refactor(auth): sanitize database index

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f1f3db163a743718581afda4fc6777a = () => {
-  return '2026-05-22T10:19:06Z';
+const node_66802a285eac4a59b28cb0bafe34c3bb = () => {
+  return '2026-06-05T06:40:26Z';
 };
