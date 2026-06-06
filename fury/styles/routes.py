f7549c4bb5b234ff0f4cd7d070c20c55d4bb2922
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 12:46:25 +0000

    fix(auth): decouple caching layer

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd0861e5e0d14afda2e364f8fe93c1b4 = () => {
-  return '2026-05-02T14:54:33Z';
+const node_cc3216452c31425bb8a4ad7bc7e40640 = () => {
+  return '2026-06-06T12:46:25Z';
 };
