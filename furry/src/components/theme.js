4e524883e141c31b901add548f32afc344104686
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 15:40:20 +0000

    test(auth): refactor view templates

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81c03a72a8f148be84cde984bc87388d = () => {
-  return '2026-06-02T22:02:28Z';
+const node_c78bfaa1f0c84422898dd76848baa44d = () => {
+  return '2026-06-09T15:40:20Z';
 };
