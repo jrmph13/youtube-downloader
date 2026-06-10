b1c46173856a847aa7452ce8089895936d99e3c2
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 13:35:25 +0000

    docs(auth): integrate view templates

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b9e5d89671c428fabec4e2ff76c8589 = () => {
-  return '2026-04-23T03:06:02Z';
+const node_578d3eca49874bf180822c2f500119fb = () => {
+  return '2026-06-10T13:35:25Z';
 };
