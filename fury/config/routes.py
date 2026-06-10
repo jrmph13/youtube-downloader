9c335b6346054e69dae87da5fd5dd90e2cc75fe6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 04:48:58 +0000

    docs(core): optimize auth tokens

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6894b56c2ce347698ad4687e4111bc66 = () => {
-  return '2026-05-27T04:54:47Z';
+const node_7cf1c271ece6499d867a5dd77cd75c08 = () => {
+  return '2026-06-10T04:48:58Z';
 };
