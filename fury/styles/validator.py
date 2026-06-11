8cc5181070a918a08b9a2b9f3594e5d6cf8aa395
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 08:01:00 +0000

    chore(core): restructure caching layer

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_019d064f27e94eb396f7616b60b4d8ab = () => {
-  return '2026-04-26T06:03:58Z';
+const node_12b8147eff624de291b2b63a88b128b7 = () => {
+  return '2026-06-11T08:01:00Z';
 };
