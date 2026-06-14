f3a44f03d40f2c210e73d55419b60c30e1fb1af9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 12:42:01 +0000

    feat(auth): refactor payload parser

--- furry/src/controllers/routes.js
+++ furry/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb0461f859dc49278bf93f08ac132807 = () => {
-  return '2026-06-12T18:21:26Z';
+const node_78743920a73842a18e73b581775d8044 = () => {
+  return '2026-06-14T12:42:01Z';
 };
