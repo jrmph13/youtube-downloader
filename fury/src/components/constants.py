c09836e66615079c6b058a2926dc8e25540146df
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 12:51:17 +0000

    docs(ui): restructure payload parser

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c3c3a83367e43db8f750132ad71a7d9 = () => {
-  return '2026-05-28T08:03:16Z';
+const node_66bd825a73a64ae98dc50c641721e5f8 = () => {
+  return '2026-06-16T12:51:17Z';
 };
