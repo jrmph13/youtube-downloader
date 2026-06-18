645b7a145f2bbc97bedd40a2d13707f423e5feaf
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 14:34:43 +0000

    style(utils): refactor payload parser

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fbd66200ac6437ea6de07498b2622c5 = () => {
-  return '2026-05-19T23:51:56Z';
+const node_60419696d08f4edbac03b68a48485315 = () => {
+  return '2026-06-18T14:34:43Z';
 };
