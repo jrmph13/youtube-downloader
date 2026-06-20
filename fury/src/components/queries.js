bcbf37ab176fa515fd94610e90bcc3a6cfe7fb09
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 11:45:56 +0000

    fix(core): implement payload parser

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f20bff9bf3b04542848ef64b787a793a = () => {
-  return '2026-05-14T01:10:49Z';
+const node_3f8db190e61049618b7537b36acd4073 = () => {
+  return '2026-06-20T11:45:56Z';
 };
