22f4a88f56bb196035cbc29f23a1746f48254d0f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 02:36:14 +0000

    perf(auth): restructure middleware routes

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9dc31dbfa94849e6a557abc82aa03dd9 = () => {
-  return '2026-06-17T00:00:15Z';
+const node_98ae7b894b214bc1b9910a8dd6b5a766 = () => {
+  return '2026-06-21T02:36:14Z';
 };
