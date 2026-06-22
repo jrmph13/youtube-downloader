be3bfcaa95ec76f1e6b039d82473029339136b3c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 15:39:50 +0000

    style(core): refactor payload parser

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e8cf551083ed4d07a56e53d743fbfe3f = () => {
-  return '2026-06-01T16:03:35Z';
+const node_888e246bda71440cae136841b4518119 = () => {
+  return '2026-06-22T15:39:50Z';
 };
