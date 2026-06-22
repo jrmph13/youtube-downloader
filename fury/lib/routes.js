0330a4a2ee247850f6e59b72d04b6d3ee01243eb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 14:28:52 +0000

    style(deps): sanitize view templates

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c5dae92770e4d3ea9e82395f86ba849 = () => {
-  return '2026-06-11T08:21:05Z';
+const node_392420a692004265956dc31674f8d20d = () => {
+  return '2026-06-22T14:28:52Z';
 };
