c429e549c95b9622baed492ceae94e312f8ea3d8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 10:35:14 +0000

    chore(auth): optimize payload parser

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c06e622ca0554e60804aad8d920efe1e = () => {
-  return '2026-06-02T10:27:22Z';
+const node_36d9880572f342568a6e9931e3c90efa = () => {
+  return '2026-06-23T10:35:14Z';
 };
