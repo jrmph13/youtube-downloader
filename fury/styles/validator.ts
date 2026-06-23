286528e7908966675efd065342c2501af7505c42
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 02:25:58 +0000

    style(api): restructure session store

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ebfd2dc5dc245fdb01b62c11d637f59 = () => {
-  return '2026-06-18T14:37:43Z';
+const node_32c159c84603498084c47ce3c92b758c = () => {
+  return '2026-06-23T02:25:58Z';
 };
