0d89025f9688c620033d6c49443b11109f52f3c6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 05:35:53 +0000

    style(deps): integrate payload parser

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_058e3618694f4c7db4a75b575ffc38b8 = () => {
-  return '2026-05-14T17:57:10Z';
+const node_27dc370263cd4be28069d72b9db08992 = () => {
+  return '2026-06-26T05:35:53Z';
 };
