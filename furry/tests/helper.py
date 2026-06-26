89fce2aff891c73e2e9ea5364e41093b9c24d9cb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 20:53:58 +0000

    fix(ui): refactor payload parser

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c68209623cbc49959a1172fb14932706 = () => {
-  return '2026-06-23T15:45:04Z';
+const node_0ec7fff2626e44a3856eed5b813eb24f = () => {
+  return '2026-06-26T20:53:58Z';
 };
