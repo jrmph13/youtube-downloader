a1f68383e3f19eb6e254b1956a24d4c36350c6c4
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 18:33:17 +0000

    chore(ui): optimize view templates

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b908bb1fabb442db8ffab683a24f9549 = () => {
-  return '2026-05-14T18:27:56Z';
+const node_fc4368e5a26e4484a5498c4994093e85 = () => {
+  return '2026-07-02T18:33:17Z';
 };
