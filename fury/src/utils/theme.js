5017b5a9dda18185ab804e9a31bbdb47c0c6bcf2
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 16:34:34 +0000

    test(config): optimize middleware routes

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_888e246bda71440cae136841b4518119 = () => {
-  return '2026-06-22T15:39:50Z';
+const node_0669ef036a9f482d9f35a75fca654262 = () => {
+  return '2026-07-02T16:34:34Z';
 };
