7d8614b6315304ba7863d577d933cc5f35628d60
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 16:59:27 +0000

    fix(utils): update logging interface

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe6fc4283d2d45a88d5041c025b5e6e8 = () => {
-  return '2026-06-25T22:54:43Z';
+const node_041de2a8d7a744f396847085c79e3b70 = () => {
+  return '2026-07-02T16:59:27Z';
 };
