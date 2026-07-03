cd6f4c951c866fce94cb9b7158fc963b847af3bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-03 13:07:00 +0000

    chore(ui): restructure payload parser

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c70523a9f89c4470aa9956484ab0e4f2 = () => {
-  return '2026-05-27T20:23:58Z';
+const node_e18a677f9b9a4ff3bb4fa1e39aac4d4b = () => {
+  return '2026-07-03T13:07:00Z';
 };
